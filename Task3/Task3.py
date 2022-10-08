d52a934e7c4102fc37ca0bb8eabcb4136617e8dd
SilverXuz <silver-xuz@mail.ru>
2022-10-08 14:00:33 +0300

    Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.

---
+++ Task3/Task3.py
@@ -0,0 +1,26 @@
+"""
+Задайте последовательность чисел. 
+Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+"""
+
+
+
+lst = [3, 7, 'nine', 3, 5, 'nine', 'four', 7, 2, 1, 4]
+print(f'Исходный список: {lst}')
+enpty_lst = []
+
+def double_num(lst, value):
+    count = 0
+    for i in range(len(lst)):
+        if lst[i] == value:
+            count += 1
+    return count
+
+def save_num(lst):
+    enpty_lst = []
+    for i in range(len(lst)):
+        if double_num(lst, lst[i]) == 1:
+            enpty_lst.append(lst[i])
+    return enpty_lst
+
+print(f'Список уникальных значений: {save_num(lst)}')
